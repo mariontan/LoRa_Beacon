603906549deddfbd62df438a12d772dd56ce25bc
Dominic Solpico <4.0.gigadertz@gmail.com>
2019-02-13 15:24:34 +0800

    Added getGPSdata.py (for storing beacon data into CSV files) and modified beaconrx.py

--- beaconrx.py
+++ beaconrx.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+from pytz import timezone
 import select
 import serial
 import sqlite3
@@ -23,8 +24,10 @@ def read_data(node):
 def split_data(data):
     data_string = ''.join(data)
     data_string = data_string.split(';')
-    mydt = datetime.strptime(data_string[0], "%d%m%y,%H%M%S.%f")
-    data_string[0] = mydt.strftime("%Y-%m-%d %H:%M:%S")
+    mydt = datetime.strptime(data_string[0], "%d%m%y,%H%M%S")
+    mydt = timezone('UTC').localize(mydt)
+    currentime = mydt.astimezone(timezone('Asia/Manila'))	
+    data_string[0] = currentime.strftime("%Y-%m-%d %H:%M:%S")
     return data_string
 
 
@@ -33,14 +36,19 @@ def save_data(data):
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS beacon
         (   id		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	    datetime    TEXT NOT NULL,
+	    datetime    DATETIME NOT NULL,
 	    node	INTEGER NOT NULL,
             rssi   	INTEGER NOT NULL,
             lat       	TEXT NOT NULL,
-            long       	TEXT NOT NULL
+	    nsd         TEXT NOT NULL,	
+            long       	TEXT NOT NULL,
+	    ewd         TEXT NOT NULL,
+            alt         TEXT NOT NULL,
+            hdop        TEXT NOT NULL,
+	    msg         TEXT 	
         );
         ''')
-    c.execute("INSERT INTO beacon VALUES(NULL,?,?,?,?,?);",data)
+    c.execute("INSERT INTO beacon VALUES(NULL,?,?,?,?,?,?,?,?,?,?);",data)
     conn.commit()
     c.execute("SELECT * FROM beacon WHERE id = (SELECT MAX(id) from beacon);")
     print(c.fetchall())
@@ -50,7 +58,10 @@ def save_data(data):
 node_1 = serial.Serial('/dev/ttyACM0', 9600)
 select.select([],[],[],20.0)
 while True:
-    data_returned = read_data(node_1)
-    data_returned = split_data(data_returned)
-    save_data(data_returned)
+    	data_returned = read_data(node_1)
+    	try:
+    		data_returned = split_data(data_returned)
+    		save_data(data_returned)
+	except Exception as e:
+		print(e)
 
